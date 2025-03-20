eca7ad2ccef4121a27a6213c1d5bc3f8d8c01298
Douglas Silva <douglasitpro@gmail.com>
2025-03-20 17:36:36 -0300

    Criado models Evendo, Convidado, Confirmado

--- evento/models.py
+++ evento/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Evento(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True)
+    data = models.DateTimeField()
+    local = models.CharField(max_length=255)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+    
+
+class Convidado(models.Model):
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(blank=False, null=False, unique=True, max_length=11)
+    email = models.CharField(blank=False, null=False, unique=True, max_length=50)
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nome} ({self.email})"
+    
+class Confirmacao(models.Model):
+    convidado = models.OneToOneField(Convidado, on_delete=models.CASCADE)
+    confirmado = models.BooleanField(default=False)
+    restricoes_alimentares = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.convidado.nome} - {'Confirmado' if self.confirmado else 'Não Confirmado'}"
