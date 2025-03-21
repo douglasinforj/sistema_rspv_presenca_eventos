65b2bef55365d1a0d9eff5b249c233108651ca53
Douglas Silva <douglasitpro@gmail.com>
2025-03-21 15:11:27 -0300

    Adicionando rota home

--- evento/urls.py
+++ evento/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.lista_eventos, name='lista_eventos'),
+    path('', views.home, name='home'),
+    path('eventos', views.lista_eventos, name='lista_eventos'),
     path('evento/<int:evento_id>/', views.detalhes_evento, name='detalhes_evento'),
     path('rsvp/<int:convidado_id>/', views.rsvp, name='rsvp'),
 ]
