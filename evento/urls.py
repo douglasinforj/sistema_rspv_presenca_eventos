72a2f07e544bfc3c638289e4d17634b76e3e451b
Douglas Silva <douglasitpro@gmail.com>
2025-03-21 12:07:52 -0300

    Reparação no codigo

--- evento/urls.py
+++ evento/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.lista_eventos, name='lista_eventos'),
     path('evento/<int:evento_id>/', views.detalhes_evento, name='detalhes_evento'),
-    path('rsvp/<int:convidado>/', views.rsvp, name='rsvp'),
+    path('rsvp/<int:convidado_id>/', views.rsvp, name='rsvp'),
 ]
