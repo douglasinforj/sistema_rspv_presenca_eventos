ee0235389f35581fb46532f0ea0e138f769974e2
Douglas Silva <douglasitpro@gmail.com>
2025-03-21 11:32:44 -0300

    Adicionando views para rsvp

--- evento/views.py
+++ evento/views.py
@@ -11,3 +11,13 @@ def detalhes_evento(request, evento_id):
     convidados = Convidado.objects.filter(evento=evento)
     return render(request, 'evento/detalhes_evento.html', {'evento': evento, 'convidados': convidados})
 
+def rsvp(request, convidado_id):
+    convidado = get_object_or_404(Convidado, id=convidado_id)
+    if request.method == 'POST':
+        form = RSVPForm(request.POST, instance=convidado.confirmacao)
+        if form.is_valid():
+            form.save()
+            return redirect('detalhes_envido', evento_id=convidado.evneto.id)
+        else:
+            form = RSVPForm(instance=convidado.confirmacao)
+        return render(request, 'evento/rsvp.html', {'form': form, 'convidado': convidado})
