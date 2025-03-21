3e3ee06d142217ba8d20600bce4b3a2f29e849c2
Douglas Silva <douglasitpro@gmail.com>
2025-03-21 10:37:55 -0300

    Adicionando rota detalhes_evento

--- evento/urls.py
+++ evento/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.lista_eventos, name='lista_eventos')
+    path('', views.lista_eventos, name='lista_eventos'),
+    path('detalhes_evento/<int:evento_id>/', views.detalhes_eventos, name='detalhes_eventos'),
 ]
