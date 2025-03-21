deceac57d7d1078c65ccf5b71a70b7cbc3ffdf57
Douglas Silva <douglasitpro@gmail.com>
2025-03-21 10:00:34 -0300

    Adicionando views listar_eventos

--- evento/views.py
+++ evento/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Evento, Convidado, Confirmacao
+from .forms import RSVPForm
 
-# Create your views here.
+def lista_eventos(request):
+    eventos = Evento.objects.all()
+    return render(render, 'evento/lista_eventos.html')
+
+
+
