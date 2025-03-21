55d5207ac60b1e42e166f60f7330e3ecff8beada
Douglas Silva <douglasitpro@gmail.com>
2025-03-21 14:47:57 -0300

    adicionando view home

--- evento/views.py
+++ evento/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Evento, Convidado, Confirmacao
 from .forms import RSVPForm
 
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+
+
 def lista_eventos(request):
     eventos = Evento.objects.all()
     return render(request, 'evento/lista_eventos.html', {'eventos': eventos})
