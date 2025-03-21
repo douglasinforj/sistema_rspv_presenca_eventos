d1695fd107460be741649ad4b0dc3a6713116c10
Douglas Silva <douglasitpro@gmail.com>
2025-03-21 10:23:41 -0300

    Ajustando erro de escrita no codigo

--- evento/forms.py
+++ evento/forms.py
@@ -1,7 +1,7 @@
 from django import forms 
 from .models import Confirmacao
 
-class RSVPForm(forms.modelsForm):
+class RSVPForm(forms.ModelForm):
     class Meta:
         model = Confirmacao
         fields = ['confirmado', 'restricoes_alimentares']
