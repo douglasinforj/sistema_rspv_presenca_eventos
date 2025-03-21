6c313a1fbcadedb35c52e6faff09ccc1b25b0e06
Douglas Silva <douglasitpro@gmail.com>
2025-03-21 19:35:39 -0300

    Adicionado rota para importar convidados

--- evento/urls.py
+++ evento/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('evento/<int:evento_id>/', views.detalhes_evento, name='detalhes_evento'),
     path('rsvp/<int:convidado_id>/', views.rsvp, name='rsvp'),
     path('cadastrar_convidado/', views.cadastrar_convidado, name='cadastrar_convidado'),
+    path("importar_convidados/", views.importar_convidados, name="importar_convidados"),
 ]
