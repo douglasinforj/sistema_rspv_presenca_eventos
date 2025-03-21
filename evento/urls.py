e78ac6767d09d260450bfcef2ce881827b46cb18
Douglas Silva <douglasitpro@gmail.com>
2025-03-21 11:33:46 -0300

    Adicionando rota rsvp

--- evento/urls.py
+++ evento/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.lista_eventos, name='lista_eventos'),
     path('evento/<int:evento_id>/', views.detalhes_evento, name='detalhes_evento'),
+    path('rsvp/<int:convidado>/', views.rsvp, name='rsvp'),
 ]
