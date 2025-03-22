c1b6c26c5b98845d0cf7a62d6721cc31090d3901
Douglas Silva <douglasitpro@gmail.com>
2025-03-22 11:54:03 -0300

    Adicionado rotas rsvp_convidado, atendente, sucesso

--- evento/urls.py
+++ evento/urls.py
@@ -6,7 +6,17 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('eventos', views.lista_eventos, name='lista_eventos'),
     path('evento/<int:evento_id>/', views.detalhes_evento, name='detalhes_evento'),
+    
+    #TODO: vai ser preciso retirar não vai fazer sentido
+    path('rsvp/convidado/', views.rsvp_convidado, name='rsvp_convidado'),
+    path('rsvp/atendente/<int:convidado_id>/', views.rsvp_atendente, name='rsvp_atendente'),
+    path('rsvp/sucesso/', views.rsvp_sucesso, name='rsvp_sucesso'),
+
     path('rsvp/<int:convidado_id>/', views.rsvp, name='rsvp'),
     path('cadastrar_convidado/', views.cadastrar_convidado, name='cadastrar_convidado'),
     path("importar_convidados/", views.importar_convidados, name="importar_convidados"),
+
+
+
+
 ]
