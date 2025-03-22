87dadbe41f4de6b6964cda74bc50a0768e01e077
Douglas Silva <douglasitpro@gmail.com>
2025-03-22 13:17:33 -0300

    Criado views para adicionar eventos

--- evento/views.py
+++ evento/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Evento, Convidado, Confirmacao
-from .forms import RSVPForm, ConvidadoForm, UploadFileForm
+from .forms import RSVPForm, ConvidadoForm, UploadFileForm, EventoForm
 
 import pandas as pd
 from django.contrib import messages
@@ -16,6 +16,22 @@ def home(request):
 def lista_eventos(request):
     eventos = Evento.objects.all()
     return render(request, 'evento/lista_eventos.html', {'eventos': eventos})
+
+
+
+def adicionar_evento(request):
+    if request.method == "POST":
+        form = EventoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_eventos') 
+    else:
+        form = EventoForm()
+    
+    return render(request, 'evento/adicionar_evento.html', {'form': form})
+    
+
+
 
 
 def detalhes_evento(request, evento_id):
