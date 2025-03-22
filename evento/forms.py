4d7c5ce58af444dd6a943a56d540d19fad582c1c
Douglas Silva <douglasitpro@gmail.com>
2025-03-22 10:00:31 -0300

    Adicionando widgets ao form para manipulação do layout do formulário

--- evento/forms.py
+++ evento/forms.py
@@ -12,6 +12,14 @@ class ConvidadoForm(forms.ModelForm):
         model = Convidado 
         fields = ['nome', 'cpf','email', 'telefone', 'evento']
 
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'cpf': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+            'evento': forms.Select(attrs={'class': 'form-control'}),
+        }
+
 
 class UploadFileForm(forms.Form):
     file = forms.FileField(label="Selecione um aqruivo CSV ou Excel")
