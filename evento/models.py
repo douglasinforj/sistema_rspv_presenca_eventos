97e103ab59e415cce33c7786022588d769df9b23
Douglas Silva <douglasitpro@gmail.com>
2025-03-23 20:05:23 -0300

    Adicionando logica para gerar QRcode ao cadastro do convidado assim que for criado

--- evento/models.py
+++ evento/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
+import qrcode
+from io import BytesIO
+from django.core.files.base import ContentFile
+
 class Evento(models.Model):
     nome = models.CharField(max_length=255)
     descricao = models.TextField(blank=True)
@@ -18,14 +23,37 @@ class Convidado(models.Model):
     email = models.CharField(blank=False, null=False, unique=True, max_length=50)
     telefone = models.CharField(blank=True, null=True, max_length=11)
     evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    qrcode = models.ImageField(upload_to='convidados_qrcodes/', null=True, blank=True)
 
     def __str__(self):
         return f"{self.nome} ({self.email})"
+    
+    def generate_qrcode(self):
+        # Gerar QR Code com base no CPF
+        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+        qr.add_data(self.cpf)
+        qr.make(fit=True)
+
+        # Criar a imagem do QR Code
+        img = qr.make_image(fill='black', back_color='white')
+
+        # Salvar imagem em formato de arquivo
+        qr_file = BytesIO()
+        img.save(qr_file, 'PNG')
+        qr_file.seek(0)
+
+        # Salvar a imagem no campo ImageField
+        self.qrcode.save(f"{self.cpf}_qrcode.png", ContentFile(qr_file.read()), save=False)
+        qr_file.close()
+    
+
     
 class Confirmacao(models.Model):
     convidado = models.OneToOneField(Convidado, on_delete=models.CASCADE, null=True, blank=True)
     confirmado = models.BooleanField(default=False)
     restricoes_alimentares = models.TextField(blank=True, null=True)
+    
 
     def __str__(self):
         return f"{self.convidado.nome} - {'Confirmado' if self.confirmado else 'Não Confirmado'}"
+
