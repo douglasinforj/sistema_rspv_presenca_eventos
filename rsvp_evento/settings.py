6c1ce74f7d92493905d46bc4cadab0d6be430252
Douglas Silva <douglasitpro@gmail.com>
2025-03-23 14:39:06 -0300

    Adicionando pasta static e imagens default

--- rsvp_evento/settings.py
+++ rsvp_evento/settings.py
@@ -118,3 +118,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+FILE_UPLOAD_HANDLERS = [
+    'django.core.files.uploadhandler.MemoryFileUploadHandler',
+    'django.core.files.uploadhandler.TemporaryFileUploadHandler',
+]
+
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
