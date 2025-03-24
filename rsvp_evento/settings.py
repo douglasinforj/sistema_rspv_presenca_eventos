9b11dd12835b09e5ca4c2a85e764a69b2eae8f66
Douglas Silva <douglasitpro@gmail.com>
2025-03-24 20:50:15 -0300

    ajustando direcionamentos de login e logout

--- rsvp_evento/settings.py
+++ rsvp_evento/settings.py
@@ -150,3 +150,9 @@ FILE_UPLOAD_HANDLERS = [
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+LOGIN_REDIRECT_URL = '/'  
+LOGOUT_REDIRECT_URL = '/login/' 
+LOGIN_URL = '/login/'  
